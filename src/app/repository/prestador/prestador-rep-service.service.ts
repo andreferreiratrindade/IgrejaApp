b3b7b604b39d618e9723ed6ef85c6af6b345f85e
Andre <andreferreiratrindade@gmail.com>
2020-08-01 13:08:45 -0300

    fix

--- src/app/repository/prestador/prestador-rep-service.service.ts
+++ src/app/repository/prestador/prestador-rep-service.service.ts
@@ -216,8 +216,6 @@ export class PrestadorRepServiceService extends BaseRepository {
 
                 let lst = [];
                 result.forEach(function (doc) {
-                    debugger
-                    console.log(doc);
                     if (doc.data().locaisAtendimento) {
 
                         doc.data().locaisAtendimento.forEach(x => {
